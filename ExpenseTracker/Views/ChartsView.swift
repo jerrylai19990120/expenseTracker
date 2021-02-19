ff8ee89c414874dc5f0b32996cc004cd88ada91c
jerry <jerrylai19990120@gmail.com>
2021-02-19 15:38:00 -0500

    charts view update

--- ExpenseTracker/Views/ChartsView.swift
+++ ExpenseTracker/Views/ChartsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ChartsView: View {
     var body: some View {
@@ -14,7 +15,13 @@ struct ChartsView: View {
             VStack {
                 
                 VStack {
-                    Text("hi")
+                    MultiLineChartView(data: [([8,32,11,23,40,28], GradientColors.green), ([90,99,78,111,70,60,77], GradientColors.purple), ([34,56,72,38,43,100,50], GradientColors.orngPink)], title: "Title")
+                    
+                    LineView(data: [8,23,54,32,12,37,7,23,43], title: "Line chart", legend: "Full screen") // legend is optional, use optional .padding()
+                    
+                    BarChartView(data: ChartData(values: [("2018 Q4",63150), ("2019 Q1",50900), ("2019 Q2",77550), ("2019 Q3",79600), ("2019 Q4",92550)]), title: "Sales", legend: "Quarterly") // legend is optional
+                    
+                    PieChartView(data: [8,23,54,32], title: "Title", legend: "Legendary") // legend is optional
                 }
             }
         }
