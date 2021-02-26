2e65a478d8a165b879de612e34e6b0a63493a68d
jerry <jerrylai19990120@gmail.com>
2021-02-25 23:04:37 -0500

    firebase integration

---
+++ ExpenseTracker/Services/AuthService.swift
@@ -0,0 +1,18 @@
+//
+//  AuthService.swift
+//  ExpenseTracker
+//
+//  Created by Jerry Lai on 2021-02-25.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import Foundation
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    
+    
+    
+}
